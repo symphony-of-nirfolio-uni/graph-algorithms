2eae034bd3c8c62904bd9194146d148bd06ece74
NKM6795 <31067906+NKM6795@users.noreply.github.com>
2019-04-10 14:42:15 +0300

    Added data and Graph_algo

---
+++ Graph_algorithms/Graph_algorithms/Graph_algo.cpp
@@ -0,0 +1,14 @@
+#include "Graph_algo.h"
+
+
+Graph_algo::Graph_algo()
+{
+
+}
+
+
+Graph_algo::~Graph_algo()
+{
+
+}
+
---
+++ Graph_algorithms/Graph_algorithms/Graph_algo.h
@@ -0,0 +1,22 @@
+#pragma once
+
+
+#include "GraphAPI.h"
+#include <vector>
+#include <string>
+
+
+using std::vector;
+using std::pair;
+using std::string;
+
+
+class Graph_algo
+{
+
+public:
+	Graph_algo();
+	
+	~Graph_algo();
+};
+
