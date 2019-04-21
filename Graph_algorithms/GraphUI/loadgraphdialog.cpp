032e906aa6f39a2f44a54e7a0aa223535a769c32
Afohtim <afohmouth@gmail.com>
2019-04-21 05:03:18 +0300

    check .dat files in data and show them in list

--- Graph_algorithms/GraphUI/loadgraphdialog.cpp
+++ Graph_algorithms/GraphUI/loadgraphdialog.cpp
@@ -1,6 +1,7 @@
 #include "loadgraphdialog.h"
 #include "ui_loadgraphdialog.h"
 #include "graphwindow.h"
+#include <QDir>
 
 LoadGraphDialog::LoadGraphDialog(QWidget *parent) :
     QDialog(parent),
@@ -21,6 +22,13 @@ LoadGraphDialog::~LoadGraphDialog()
 
 void LoadGraphDialog::load_graphs()
 {
+    QDir *dir = new QDir("../Graph_algorithms/Graph_data");
+    auto list = dir->entryInfoList();
+    for(auto file : list)
+    {
+        if(file.suffix() == "dat")
+            ui->graphList->addItem(file.baseName());
+    }
     ui->graphList->addItem("test");
 
     ui->graphList->setCurrentItem((ui->graphList->item(0)));
