944159df549a60a8332fae191af9bf82aac2b256
Afohtim <afohmouth@gmail.com>
2019-04-21 15:19:34 +0300

    api creation change, get coordinates, get graph from file methods demo versions

--- Graph_algorithms/Graph_algorithms/GraphAPI.cpp
+++ Graph_algorithms/Graph_algorithms/GraphAPI.cpp
@@ -2,6 +2,24 @@
 
 
 
+vector<pair<double, double> > GraphAPI::get_vertices_coodrdinates(std::string graph_file_name)
+{
+    //TODO: make a real implementation
+
+    return {{0, 6},
+            {2, 4},
+            {3, 8},
+            {7, 4},
+        {10, 5}};
+}
+
+algorithms_on_graphs::Graph GraphAPI::get_graph(std::string graph_file_name)
+{
+    //TODO: make a real implementation
+
+    return Graph({{},{{2,0}},{{1,0}},{{4,0}},{{3,0}}});
+}
+
 GraphAPI::GraphAPI()
 {
 	
