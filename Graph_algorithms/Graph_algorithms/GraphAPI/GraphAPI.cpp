9bf52b619e6d3ea611fb0bdf9f22fa0120e0f7c7
Артемий Мысечко <mysechko01@gmail.com>
2019-04-22 22:41:20 +0300

    new get verteces coordinates function

--- Graph_algorithms/Graph_algorithms/GraphAPI/GraphAPI.cpp
+++ Graph_algorithms/Graph_algorithms/GraphAPI/GraphAPI.cpp
@@ -8,17 +8,40 @@ vector<Point> GraphAPI::get_vertices_coordinates(std::string graph_file_name)
 {
 	algorithms_on_graphs::Graph graph = get_graph(graph_file_name);
 
-	vector<Point> position;
+	vector<Point> positions;
+	if (graph.get_size() == 0)
+		return positions;
 
 	double cell_count = graph.get_size();
 	double angle = 360.0 / cell_count;
-	//double radius = 
-	for (int i = 0; i < graph.get_size(); ++i)
+	double radius = 49 - 49 / cell_count;
+	Point mainDot(0, radius);
+	for (int i = 0; i < graph.get_size()/2 + graph.get_size()%2; ++i)
 	{
-
+		positions.push_back(build_dots::getDot(mainDot, angle, i, radius));
+	}
+	Point tempDot;
+	if (graph.get_size() % 2 == 1)
+	{
+		for (int i = int(positions.size() - 1); i >= graph.get_size() % 2; --i)
+		{
+			tempDot = positions[i];
+			tempDot.x = -tempDot.x;
+			positions.push_back(tempDot);
+		}
+	}
+	else
+	{
+		for (int i = int(positions.size() - 1); i >= 0; --i)
+		{
+			tempDot = positions[i];
+			tempDot.x = -tempDot.x;
+			tempDot.y = -tempDot.y;
+			positions.push_back(tempDot);
+		}
 	}
 
-	return position;
+	return positions;
 }
 
 algorithms_on_graphs::Graph GraphAPI::get_graph(std::string graph_file_name)
