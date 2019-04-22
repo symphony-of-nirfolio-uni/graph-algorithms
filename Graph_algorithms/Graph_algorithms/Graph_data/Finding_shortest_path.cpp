37ab542373be9453b6620e0eab89f925c71068fd
NKM6795 <31067906+NKM6795@users.noreply.github.com>
2019-04-22 23:48:23 +0300

    Wrote Finding_shortest_path work

--- Graph_algorithms/Graph_algorithms/Graph_data/Finding_shortest_path.cpp
+++ Graph_algorithms/Graph_algorithms/Graph_data/Finding_shortest_path.cpp
@@ -3,9 +3,74 @@
 
 namespace algorithms_on_graphs
 {
+	void Finding_shortest_path::bfs(Graph &graph, queue<int> &next_verteces, vector<int> &direction, int finish, bool need_to_stop)
+	{
+		while (!next_verteces.empty())
+		{
+			int vertex = next_verteces.front();
+			next_verteces.pop();
+
+			//GraphAPI part
+			if (need_to_stop)
+			{
+				//GraphAPI::getInstance().highlight_vertex(vertex);
+				//waiting_for_the_next_move();
+			}
+			//
+
+			for (auto new_vertex : graph.at(vertex).adjacent())
+			{
+				if (direction[new_vertex] != -1)
+				{
+					next_verteces.push(new_vertex);
+					direction[new_vertex] = vertex;
+
+					if (new_vertex == finish)
+					{
+						return;
+					}
+				}
+			}
+
+			//GraphAPI part
+			if (need_to_stop)
+			{
+				//GraphAPI:::getInstance().mark_vertex(vertex);
+				//waiting_for_the_next_move();
+			}
+			//
+		}
+	}
+
+
+	Graph Finding_shortest_path::create_route(Graph &graph, vector<int> &direction, int start, int finish)
+	{
+		vector<vector<int> > new_graph(graph.get_size());
+
+		int index = finish;
+		
+		while (direction[index] != start)
+		{
+			new_graph[direction[index]].push_back(index);
+
+			index = direction[index];
+		}
+
+		return Graph(new_graph, true);
+	}
+
+
 	void Finding_shortest_path::work(Graph graph, int start, int finish, bool need_to_stop)
 	{
-		//TODO work
+		vector<int> direction(graph.get_size(), -1);
+
+		queue<int> next_verteces;
+		next_verteces.push(start);
+		direction[start] = start;
+
+		bfs(graph, next_verteces, direction, finish, need_to_stop);
+
+
 	}
 
 }
