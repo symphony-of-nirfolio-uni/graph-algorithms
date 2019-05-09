a6a44cd924efbed3dba47ef51b80a9e618f24677
Артемий Мысечко <mysechko01@gmail.com>
2019-05-09 18:10:27 +0300

    time of work function added

--- Graph_algorithms/Graph_algorithms/Control_data_time/Control_data_time.cpp
+++ Graph_algorithms/Graph_algorithms/Control_data_time/Control_data_time.cpp
@@ -1,1 +1,19 @@
 #include "Control_data_time.h"
+
+namespace control_data_time 
+{
+	string timeOfWork()//(GraphAPI::Algorithm, Graph graph)		need new version
+	{
+		std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
+		//GraphAPI::start_algorithm_nonstop();
+		std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
+		double duration = double(std::chrono::duration_cast<std::chrono::microseconds>(start - end).count());
+		string result = " mcs";
+		if (duration/1000.0 > 10.0)
+		{
+			duration = duration / 1000.0;
+			result = " ms";
+		}
+		return std::to_string(double(duration)) + result;
+	}
+}
--- Graph_algorithms/Graph_algorithms/Control_data_time/Control_data_time.h
+++ Graph_algorithms/Graph_algorithms/Control_data_time/Control_data_time.h
@@ -1,9 +1,12 @@
 #pragma once
 #include "../Graph_data/Graph.h"
-
-using algorithms_on_graphs::Graph;
+#include "../GraphAPI/GraphAPI.h"
+#include <chrono>
+#include <string>
 
 namespace control_data_time
 {
+	using algorithms_on_graphs::Graph;
 
+	string timeOfWork();//(GraphAPI::Algorithm, Graph graph)		need new version
 }
