33d3311f4f42b69b41f866f8f4e99f670dd41163
Afohtim <afohmouth@gmail.com>
2019-05-10 12:09:31 +0300

    ui main code clear

--- Graph_algorithms/GraphUI/main.cpp
+++ Graph_algorithms/GraphUI/main.cpp
@@ -6,16 +6,9 @@
 
 int main(int argc, char *argv[])
 {
-    try {
-        QApplication a(argc, argv);
-        MainWindow w;
-        w.show();
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
 
-        a.exec();
-        std::terminate();
-    } catch (std::exception e) {
-        MessageDialog *win = new MessageDialog(QString::fromStdString(e.what()));
-        win->exec();
-    }{}
-    return 0;
+    return a.exec();
 }
