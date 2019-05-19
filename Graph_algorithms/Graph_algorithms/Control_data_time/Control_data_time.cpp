3ec8c02f87f282b2c8c414719eb41c679b0270fc
Артемий Мысечко <mysechko01@gmail.com>
2019-05-19 11:37:24 +0300

    dataUsed function(without Bogdan's function)

--- Graph_algorithms/Graph_algorithms/Control_data_time/Control_data_time.cpp
+++ Graph_algorithms/Graph_algorithms/Control_data_time/Control_data_time.cpp
@@ -1,7 +1,17 @@
 #include "Control_data_time.h"
 
+#include <Windows.h>
+#include <psapi.h>
+#include <thread>
+
 namespace control_data_time 
 {
+	void start_algorithm_without_stops(GraphAPI::Algorithm algorithm_name, Graph graph)
+	{
+		GraphAPI::instance().start_algorithm_without_stops(algorithm_name, graph);
+
+	}
+
 	string timeOfWork(GraphAPI::Algorithm algorithm_name, Graph graph)
 	{
 		std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
@@ -22,6 +32,28 @@ namespace control_data_time
 
 	string dataUsed(GraphAPI::Algorithm algorithm_name, Graph graph)
 	{
-		return string();
+		double start_data_usage = 168.0;
+		double max_data_usage = 0;
+		PROCESS_MEMORY_COUNTERS_EX memoryInfo;
+		ZeroMemory(&memoryInfo, sizeof(PROCESS_MEMORY_COUNTERS_EX));
+		GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&memoryInfo, sizeof(memoryInfo));
+		start_data_usage += (double)memoryInfo.PrivateUsage / 1024.0;
+		
+		std::thread control_data(start_algorithm_without_stops, algorithm_name, graph);
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		
+		control_data.detach();
+		
+		while (true)
+		{
+			//how get move
+			if (true) 
+			{//todo)
+				ZeroMemory(&memoryInfo, sizeof(PROCESS_MEMORY_COUNTERS_EX));
+				GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&memoryInfo, sizeof(memoryInfo));
+				max_data_usage = max(max_data_usage, memoryInfo.PrivateUsage / 1024.0 - start_data_usage);
+			}
+		}
+		return std::to_string((double)max_data_usage) + " KB";
 	}
 }
