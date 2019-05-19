2be3d704a6b8680e6838cc98ae937b8bef495177
Артемий Мысечко <mysechko01@gmail.com>
2019-05-19 13:27:51 +0300

    dataUsed and timeOfWork have to work

--- Graph_algorithms/Graph_algorithms/Control_data_time/Control_data_time.cpp
+++ Graph_algorithms/Graph_algorithms/Control_data_time/Control_data_time.cpp
@@ -6,18 +6,17 @@
 
 namespace control_data_time 
 {
-	/*
-	void start_algorithm(GraphAPI::Algorithm algorithm_name, Graph graph, int &can_move)
+	void start_algorithm_for_data(GraphAPI::Algorithm algorithm_name, Graph graph, int &can_move)
 	{
 		GraphAPI::instance().start_algorithm(algorithm_name, graph, can_move);
 
 	}
 
-	void start_algorithm(GraphAPI::Algorithm algorithm_name, Graph graph, int start, int finish, bool &can_move_on)
+	void start_algorithm_for_data_start_finish(GraphAPI::Algorithm algorithm_name, Graph graph, int start, int finish, int &can_move)
 	{
 		GraphAPI::instance().start_algorithm(algorithm_name, graph, start, finish, can_move);
 
-	}*/
+	}
 
 	string timeOfWork(GraphAPI::Algorithm algorithm_name, Graph graph)
 	{
@@ -57,8 +56,6 @@ namespace control_data_time
 
 	string dataUsed(GraphAPI::Algorithm algorithm_name, Graph graph)
 	{
-		///wait changes from bogdan
-		/*
 		double start_data_usage = 168.0;
 		double max_data_usage = 0;
 		int can_move = 0;
@@ -67,14 +64,13 @@ namespace control_data_time
 		GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&memoryInfo, sizeof(memoryInfo));
 		start_data_usage += (double)memoryInfo.PrivateUsage / 1024.0;
 		
-		std::thread control_data(start_algorithm, algorithm_name, graph, can_move);
+		std::thread control_data(start_algorithm_for_data, algorithm_name, graph, std::ref(can_move));
 		std::this_thread::sleep_for(std::chrono::milliseconds(10));
 		
 		control_data.detach();
 		
-		/*while (can_move != -1)
+		while (can_move != -1)
 		{
-			//how get move
 			if (can_move == 0) 
 			{
 				ZeroMemory(&memoryInfo, sizeof(PROCESS_MEMORY_COUNTERS_EX));
@@ -82,14 +78,12 @@ namespace control_data_time
 				max_data_usage = max(max_data_usage, memoryInfo.PrivateUsage / 1024.0 - start_data_usage);
 				can_move = 1;
 			}
-		}*/
-		return "0 KB";//std::to_string((double)max_data_usage) + " KB";
+		}
+		return std::to_string((double)max_data_usage) + " KB";
 	}
 
 	string dataUsed(GraphAPI::Algorithm algorithm_name, Graph graph, int start, int finish)
 	{
-		///wait changes from bogdan
-		/*
 		double start_data_usage = 168.0;
 		double max_data_usage = 0;
 		int can_move = 0;
@@ -98,14 +92,13 @@ namespace control_data_time
 		GetProcessMemoryInfo(GetCurrentProcess(), (PROCESS_MEMORY_COUNTERS*)&memoryInfo, sizeof(memoryInfo));
 		start_data_usage += (double)memoryInfo.PrivateUsage / 1024.0;
 
-		std::thread control_data(start_algorithm, algorithm_name, graph, start, finish, can_move);
+		std::thread control_data(start_algorithm_for_data_start_finish, algorithm_name, graph, start, finish, std::ref(can_move));
 		std::this_thread::sleep_for(std::chrono::milliseconds(10));
 
 		control_data.detach();
 
 		while (can_move != -1)
 		{
-			//how get move
 			if (can_move == 0)
 			{
 				ZeroMemory(&memoryInfo, sizeof(PROCESS_MEMORY_COUNTERS_EX));
@@ -113,7 +106,7 @@ namespace control_data_time
 				max_data_usage = max(max_data_usage, memoryInfo.PrivateUsage / 1024.0 - start_data_usage);
 				can_move = 1;
 			}
-		}*/
-		return "0 KB";//std::to_string((double)max_data_usage) + " KB";
+		}
+		return std::to_string((double)max_data_usage) + " KB";
 	}
 }
