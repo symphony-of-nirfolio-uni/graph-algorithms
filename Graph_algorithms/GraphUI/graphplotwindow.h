1b94f0c6d2a7c6ee3cdd29797328b3c2c1f0d85a
Afohtim <afohmouth@gmail.com>
2019-05-20 04:31:53 +0300

    added time stats, slider for time control, time algo fix

--- Graph_algorithms/GraphUI/graphplotwindow.h
+++ Graph_algorithms/GraphUI/graphplotwindow.h
@@ -43,6 +43,7 @@ private:
     vector<QCPItemLine*> lines;
     vector<QCPItemLine*> arrows;
     QTimer *timer;
+    QTime *time_registrer;
     vector<unsigned> black_v;
     vector<unsigned> used_v;
     status current_status;
@@ -55,6 +56,7 @@ private:
 
     QString graph_name;
     QString algo_result;
+    QString algo_stats;
     double scatter_radius;
     int plot_min;
     int plot_max;
@@ -67,11 +69,15 @@ private:
     void setup_update_timer();
     void setup_buttons();
     void setup_algo_list();
+    void setup_slider();
     void add_used_vertex(unsigned vertex);
     void add_black_vertex(unsigned vertex);
     void make_highlighted(unsigned vertex);
     void update_status();
     void get_algo_result();
+    void get_algo_stats();
+    GraphAPI::Algorithm get_algo_enum(QString algo_name);
+
     void dots_reset();
 
     void exec_message_dialog(QString message);
@@ -79,12 +85,15 @@ private:
 
 private slots:
     void update_graph();
+    void update_timer(int);
+    void end_timer();
 
     void update_highlighted();
     void update_used();
     void update_black();
 
     void choose_algo();
+    void start_algo();
     void end_algo();
     void end_algo_mute();
 
